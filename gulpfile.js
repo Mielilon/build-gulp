96ba66508b52bca26e189dc6a7dbc00202745049
Дарья Магомедова <mielilon@MacBook-Air-Darya.local>
2022-09-19 14:57:10 +0300

    Настройка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 // Импорт пакетов
 const gulp = require('gulp')
-const less = require('gulp-less')
-const stylus = require('gulp-stylus')
+const fileinclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'))
 const rename = require('gulp-rename')
 const cleanCSS = require('gulp-clean-css')
 const ts = require('gulp-typescript')
-//const coffee = require('gulp-coffee')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
@@ -15,7 +13,7 @@ const autoprefixer = require('gulp-autoprefixer')
 const imagemin = require('gulp-imagemin')
 const htmlmin = require('gulp-htmlmin')
 const size = require('gulp-size')
-//const gulppug = require('gulp-pug')
+const gulppug = require('gulp-pug')
 const newer = require('gulp-newer')
 const browsersync = require('browser-sync').create()
 const del = require('del')
@@ -48,7 +46,7 @@ function clean() {
 // Обработка html и pug
 function html() {
   return gulp.src(paths.html.src)
-  //.pipe(gulppug())
+  .pipe(gulppug())
   .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(size({
     showFiles:true
@@ -61,8 +59,6 @@ function html() {
 function styles() {
   return gulp.src(paths.styles.src)
   .pipe(sourcemaps.init())
-  //.pipe(less())
-  //.pipe(stylus())
   .pipe(sass().on('error', sass.logError))
   .pipe(autoprefixer({
     cascade: false
@@ -82,17 +78,14 @@ function styles() {
   .pipe(browsersync.stream())
 }
 
-// Обработка Java Script, Type Script и Coffee Script
+// Обработка Java Script, Type Script
 function scripts() {
   return gulp.src(paths.scripts.src)
   .pipe(sourcemaps.init())
-  //.pipe(coffee({bare: true}))
-  /*
   .pipe(ts({
     noImplicitAny: true,
     outFile: 'main.min.js'
   }))
-  */
   .pipe(babel({
     presets: ['@babel/env']
   }))
